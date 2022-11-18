5ba32635a401d6fb1f4e735b30122168990b088c
elidafonseca <elidamabsfonseca@gmail.com>
2022-11-18 20:24:19 -0300

    commit de rotas

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import {BrowserRouter as Rota,Routes,Route } from "react-router-dom";
 import Telaprincipal from "./pages/Telaprincipal";
+import CadastroCliente from "./pages/CadastroCliente";
 
 function App() {
   return (
     <Rota>
       <Routes>
         <Route path="/principal"  element={<Telaprincipal />}/>
+        <Route path="/cadastrocliente"  element={<CadastroCliente/>}/>
       </Routes>
     </Rota>
     )
---
+++ src/pages/CadastroCliente.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function CadastroCliente()
+{
+    return (
+        <>
+        <h1>Cadastrar Cliente</h1>
+        </>
+    )
+}
